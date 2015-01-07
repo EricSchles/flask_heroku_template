918191184c51d0e0648a1cc1a3096e81a25c0da6
EricSchles <ericschles@gmail.com>
2015-01-07 09:18:01 -0500

    starting over

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
-
+import os
+from flask import flask
 app = Flask(__name__)
 
-@app.route("/",methods=["GET","POST"])
-def index():
-  return render_template("index.html")
+@app.route("/")
+def hello():
+	return "Hello World!"
 
 if __name__ == '__main__':
-  app.run(debug=True)
+	port = int(os.environ.get("PORT",5000))
+	app.run(host='0.0.0.0', port=port)
